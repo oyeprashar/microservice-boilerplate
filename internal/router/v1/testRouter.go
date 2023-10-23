fa5f14673e3e44fd0a3bdd7fdeb520ba2f4cb5e0
shubhamprashar <shubham.prashar@finbox.in>
2023-10-23 20:46:45 +0530

    added logger

--- internal/router/v1/testRouter.go
+++ internal/router/v1/testRouter.go
@@ -5,6 +5,7 @@ import (
 	"github.com/oyeprashar/microservice-boilerplate/internal/controller"
 	"github.com/oyeprashar/microservice-boilerplate/internal/handlers"
 	"github.com/oyeprashar/microservice-boilerplate/internal/middleware/auth"
+	"github.com/oyeprashar/microservice-boilerplate/internal/middleware/requestLogger"
 )
 
 // TestRouter is Router for /test pattern
@@ -14,9 +15,12 @@ func TestRouter(r chi.Router) {
 
 func testRoutes(r chi.Router) {
 
+	//r.Use(requestLogger.LogRequestResponse)
+
 	r.With(
-		auth.AuthFilter,       // This authenticates the api key,
-		controller.HealthCont, // This sets the response payload in content with key "resData"
+		auth.AuthFilter,                  // This authenticates the api key,
+		controller.HealthCont,            // This sets the response payload in content with key "resData"
+		requestLogger.LogRequestResponse, // This will log the request and response
 	).Get("/health", handlers.RespondWithSuccess)
 	// This reads "resData" from context and writes to http.ResponseWriter
 
--- main.go
+++ main.go
@@ -13,9 +13,22 @@ import (
 func main() {
 
 	/*
+		TODO: Logging the request/response
+			1. We need to pass the logger all over the code base instead of creating multiple logger object
+			2. We can then use in the the handler to log the body
+	*/
+
+	/*
+		TODO:
+			1. Complete the basic things for the service
+			2. Add features
+			3. Dockerise
+	*/
+
+	/*
 		TODO: Things to add
 			1. Handler with all the required middleware
-			2. The service should log all the request and response that are being processsed
+			2. The service should log all the request and response that are being processes
 			3. Prometheus and Grafana
 			4. MongoDB for logging the request and response
 			5. Unit test for this boiler plate (in the most organized way, probably in a single folder (Check))
@@ -34,6 +47,9 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID) // This middleware generates a unique identifier (usually a UUID or GUID)
+	//r.Use(middleware.Logger)
+	// TODO: add a middleware that prints the request and the response of the API made to us
+
 	r.Route("/v1", v1.V1Router)
 
 	// understand this code very clearly
